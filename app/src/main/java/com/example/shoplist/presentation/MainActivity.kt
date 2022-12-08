9989a6d85abd5e643c96247c3f481ba32a6ac85b
Fail Salikhov <sff1313@yandex.ru>
2022-12-08 14:05:01 +0400

    составил список и метод вывода для Linerlayuot

--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -3,20 +3,49 @@ package com.example.shoplist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoplist.R
+import com.example.shoplist.domain.ShopItem
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var llShopList : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("test", it.toString())
+            showList(it)
         }
+    }
+
+    private fun showList (list: List<ShopItem>) {
+        llShopList.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled){
+                R.layout.item_shop_enabeld
+            } else {
+                R.layout.item_shop_disabled
+            }
+
+        val view = LayoutInflater.from(this).inflate(layoutId,  llShopList, false)
+            val tvName = findViewById<TextView>(R.id.tv_name)
+            val tvCount = findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnableStare(shopItem)
+                true
+            }
+            llShopList.addView(view)
 
     }
 }
